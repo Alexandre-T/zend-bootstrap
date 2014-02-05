c208b34b8ba7aaa17df3c0c7d5ee7238f4a4d29b
Alexandre Tranchant <alexandre.tranchant@gmail.com>
2014-02-05 08:59:03 +0100

    File ommited in previous commit

---
+++ library/Bootstrap/Form/View/Helper/RadioTag.php
@@ -0,0 +1,64 @@
+<?php
+namespace Bootstrap\Form\View\Helper;
+
+use Zend\Form\View\Helper\AbstractHelper;
+use Zend\Form\ElementInterface;
+
+/**
+ *
+ * @author alexandre
+ *        
+ */
+class RadioTag extends AbstractHelper
+{
+
+    const DEFAULT_CLASS = 'radio';
+
+    /**
+     * Renders the checkbox div tag
+     *
+     * @param string $content            
+     * @return string
+     */
+    public function render($content, $class = self::DEFAULT_CLASS)
+    {
+        return $this->openTag($class) . $content . $this->closeTag();
+    }
+
+    /**
+     * Returns the form group open tag
+     *
+     * @param ElementInterface $element            
+     * @return string
+     */
+    public function openTag($class = self::DEFAULT_CLASS)
+    {
+        return '<div class="' . $class . '">';
+    }
+
+    /**
+     * Returns the control group closing tag
+     *
+     * @return string
+     */
+    public function closeTag()
+    {
+        //don't forget triming space because of form-inline separation
+        return '</div> ';
+    }
+
+    /**
+     * Invoke helper as function
+     * Proxies to {@link render()}.
+     *
+     * @param ElementInterface $element            
+     * @param string $content            
+     * @return string
+     */
+    public function __invoke($content, $class = self::DEFAULT_CLASS)
+    {
+        return $this->render($content,$class);
+    }
+}
+
+?>
