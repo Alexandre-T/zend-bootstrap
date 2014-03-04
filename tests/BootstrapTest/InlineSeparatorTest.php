1d8ec77e73a93bf2de8c14ab681b87f10cfcdf2a
Alexandre Tranchant <alexandre.tranchant@gmail.com>
2014-03-04 14:37:02 +0100

    InlineSeparatorTest pattern. todo : tests coding

---
+++ tests/BootstrapTest/InlineSeparatorTest.php
@@ -0,0 +1,102 @@
+<?php
+use Bootstrap\Form\View\Helper\InlineSeparator;
+
+/**
+ * InlineSeparator test case.
+ */
+class InlineSeparatorTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     *
+     * @var InlineSeparator
+     */
+    private $InlineSeparator;
+
+    /**
+     * Prepares the environment before running a test.
+     */
+    protected function setUp ()
+    {
+        parent::setUp();
+        
+        // TODO Auto-generated InlineSeparatorTest::setUp()
+        
+        $this->InlineSeparator = new InlineSeparator(/* parameters */);
+    }
+
+    /**
+     * Cleans up the environment after running a test.
+     */
+    protected function tearDown ()
+    {
+        // TODO Auto-generated InlineSeparatorTest::tearDown()
+        $this->InlineSeparator = null;
+        
+        parent::tearDown();
+    }
+
+    /**
+     * Constructs the test case.
+     */
+    public function __construct ()
+    {
+        // TODO Auto-generated constructor
+    }
+
+    /**
+     * Tests InlineSeparator->render()
+     */
+    public function testRender ()
+    {
+        // TODO Auto-generated InlineSeparatorTest->testRender()
+        $this->markTestIncomplete("render test not implemented");
+        
+        $this->InlineSeparator->render(/* parameters */);
+    }
+
+    /**
+     * Tests InlineSeparator->openTag()
+     */
+    public function testOpenTag ()
+    {
+        // TODO Auto-generated InlineSeparatorTest->testOpenTag()
+        $this->markTestIncomplete("openTag test not implemented");
+        
+        $this->InlineSeparator->openTag(/* parameters */);
+    }
+
+    /**
+     * Tests InlineSeparator->splitTag()
+     */
+    public function testSplitTag ()
+    {
+        // TODO Auto-generated InlineSeparatorTest->testSplitTag()
+        $this->markTestIncomplete("splitTag test not implemented");
+        
+        $this->InlineSeparator->splitTag(/* parameters */);
+    }
+
+    /**
+     * Tests InlineSeparator->closeTag()
+     */
+    public function testCloseTag ()
+    {
+        // TODO Auto-generated InlineSeparatorTest->testCloseTag()
+        $this->markTestIncomplete("closeTag test not implemented");
+        
+        $this->InlineSeparator->closeTag(/* parameters */);
+    }
+
+    /**
+     * Tests InlineSeparator->__invoke()
+     */
+    public function test__invoke ()
+    {
+        // TODO Auto-generated InlineSeparatorTest->test__invoke()
+        $this->markTestIncomplete("__invoke test not implemented");
+        
+        $this->InlineSeparator->__invoke(/* parameters */);
+    }
+}
+
